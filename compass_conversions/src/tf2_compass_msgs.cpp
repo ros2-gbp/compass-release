c5a517f291d6c39c1a3fbe343a8da71216c8c9a0
Martin Pecka <peckama2@fel.cvut.cz>
2026-08-23 06:21:21 +0200

    Rebase from 'upstream'

--- compass_conversions/src/tf2_compass_msgs.cpp
+++ compass_conversions/src/tf2_compass_msgs.cpp
@@ -19,45 +19,40 @@
 
 using Az = compass_interfaces::msg::Azimuth;
 
-namespace tf2
-{
+namespace tf2 {
 
 template<>
-tf2::TimePoint getTimestamp(const compass_interfaces::msg::Azimuth& t)
-{
+tf2::TimePoint getTimestamp(const compass_interfaces::msg::Azimuth& t) {
   return tf2::TimePoint(tf2::Duration(rclcpp::Time(t.header.stamp).nanoseconds()));
 }
 
 template<>
-std::string getFrameId(const compass_interfaces::msg::Azimuth& t)
-{
+std::string getFrameId(const compass_interfaces::msg::Azimuth& t) {
   return t.header.frame_id;
 }
 
-compass_interfaces::msg::Azimuth toMsg(const compass_interfaces::msg::Azimuth& in)
-{
+compass_interfaces::msg::Azimuth toMsg(const compass_interfaces::msg::Azimuth& in) {
   return in;
 }
 
-void fromMsg(const compass_interfaces::msg::Azimuth& msg, compass_interfaces::msg::Azimuth& out)
-{
+void fromMsg(const compass_interfaces::msg::Azimuth& msg, compass_interfaces::msg::Azimuth& out) {
   out = msg;
 }
 
 template<>
 void doTransform(
-  const compass_interfaces::msg::Azimuth& t_in, compass_interfaces::msg::Azimuth& t_out,
-  const geometry_msgs::msg::TransformStamped& transform)
-{
+    const compass_interfaces::msg::Azimuth& t_in, compass_interfaces::msg::Azimuth& t_out,
+    const geometry_msgs::msg::TransformStamped& transform) {
   t_out = t_in;
   t_out.header.frame_id = transform.header.frame_id;
   t_out.header.stamp = transform.header.stamp;
 
   auto yaw = cras::getYaw(transform.transform.rotation);
-  if (t_in.unit == Az::UNIT_DEG)
+  if (t_in.unit == Az::UNIT_DEG) {
     yaw = angles::to_degrees(yaw);
+  }
 
   t_out.azimuth += yaw * (t_in.orientation == Az::ORIENTATION_NED ? -1 : 1);
 }
 
-}
+}  // namespace tf2
--- magnetic_model/include/magnetic_model/magnetic_model.hpp
+++ magnetic_model/include/magnetic_model/magnetic_model.hpp
@@ -21,14 +21,12 @@
 #include <sensor_msgs/msg/magnetic_field.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
 
-namespace magnetic_model
-{
+namespace magnetic_model {
 
 /**
  * \brief Properties of a local magnetic field vector.
  */
-struct MagneticField
-{
+struct MagneticField {
   sensor_msgs::msg::MagneticField field;  //!< The computed magnetic field vector in ENU convention [T].
   geometry_msgs::msg::Vector3 dt;  //!< Time derivative of the field [T/year].
   geometry_msgs::msg::Vector3 error;  //!< Standard deviations of the magnetic field vector components.
@@ -37,10 +35,9 @@ struct MagneticField
 /**
  * \brief Components into which a 3D magnetic vector can be decomposed.
  */
-struct MagneticFieldComponents
-{
-  double horizontalMagnitude;  //!< Magnitude of the horizontal component [T].
-  double totalMagnitude;  //!< Total magnitude of the vector [T].
+struct MagneticFieldComponents {
+  double horizontal_magnitude;  //!< Magnitude of the horizontal component [T].
+  double total_magnitude;  //!< Total magnitude of the vector [T].
   double declination;  //!< Declination of the vector (angle from North towards East) [rad].
   double inclination;  //!< Inclination of the vector (angle from horizontal direction downwards) [rad].
 };
@@ -48,8 +45,7 @@ struct MagneticFieldComponents
 /**
  * \brief Magnetic field components together with their time derivatives and error terms.
  */
-struct MagneticFieldComponentProperties
-{
+struct MagneticFieldComponentProperties {
   MagneticFieldComponents values;  //!< Values of the components [T or rad].
   MagneticFieldComponents dt;  //!< Time derivatives of the components [T/year or rad/year].
   MagneticFieldComponents errors;  //!< Standard deviations of the components [T or rad].
@@ -63,15 +59,14 @@ struct MagneticModelPrivate;
  * The GAZEBO model is a special instance of IGRF-14 on 22/01/2018, which corresponds to the
  * baked-in data table used by the Gazebo Magnetometer system.
  */
-class MagneticModel
-{
+class MagneticModel {
 public:
   using NodeClockInterface = rclcpp::node_interfaces::NodeClockInterface;
   using NodeLoggingInterface = rclcpp::node_interfaces::NodeLoggingInterface;
 
   using RequiredInterfaces = rclcpp::node_interfaces::NodeInterfaces<
-    NodeClockInterface,
-    NodeLoggingInterface
+      NodeClockInterface,
+      NodeLoggingInterface
   >;
 
   static const char* GAZEBO;  //!< Gazebo model name
@@ -86,10 +81,10 @@ public:
    *
    * \param[in] node The node to use.
    * \param[in] name Name of the model (e.g. "wmm2020").
-   * \param[in] modelPath Path to the folder with stored models. If empty, a default system location will be used.
+   * \param[in] model_path Path to the folder with stored models. If empty, a default system location will be used.
    * \param[in] strict Whether to fail if the magnetic model is used outside its natural validity bounds.
    */
-  MagneticModel(RequiredInterfaces node, const std::string& name, const std::string& modelPath, bool strict);
+  MagneticModel(RequiredInterfaces node, const std::string& name, const std::string& model_path, bool strict);
   virtual ~MagneticModel();
 
   /**
@@ -108,38 +103,39 @@ public:
 
   /**
    * \brief Get the magnetic field vector on the provided place on Earth.
-   * \param[in] fixMsg The place for which magnetic field is queried. Timestamp from the message is ignored.
-   * \param[in] stampIn The time for which magnetic field is queried.
+   * \param[in] fix_msg The place for which magnetic field is queried. Timestamp from the message is ignored.
+   * \param[in] stamp_in The time for which magnetic field is queried.
    * \return The magnetic field.
    */
   virtual cras::expected<MagneticField, std::string> getMagneticField(
-    const sensor_msgs::msg::NavSatFix& fixMsg, const rclcpp::Time& stampIn) const;
+      const sensor_msgs::msg::NavSatFix& fix_msg, const rclcpp::Time& stamp_in) const;
 
   /**
    * \brief Get the magnetic field components on the provided place on Earth.
-   * \param[in] fixMsg The place for which magnetic field components are queried. Timestamp from the message is ignored.
-   * \param[in] stampIn The time for which magnetic field components are queried.
+   * \param[in] fix_msg The place for which magnetic field components are queried.
+   *                    Timestamp from the message is ignored.
+   * \param[in] stamp_in The time for which magnetic field components are queried.
    * \return The magnetic field components.
    */
   virtual cras::expected<MagneticFieldComponentProperties, std::string> getMagneticFieldComponents(
-    const sensor_msgs::msg::NavSatFix& fixMsg, const rclcpp::Time& stampIn) const;
+      const sensor_msgs::msg::NavSatFix& fix_msg, const rclcpp::Time& stamp_in) const;
 
   /**
    * \brief Get the components of the measured magnetic field.
    * \param[in] field The measured magnetic field.
-   * \param[in] stampIn The time for which magnetic field components are queried.
+   * \param[in] stamp_in The time for which magnetic field components are queried.
    * \return The magnetic field components.
    */
   virtual cras::expected<MagneticFieldComponentProperties, std::string> getMagneticFieldComponents(
-    const MagneticField& field, const rclcpp::Time& stampIn) const;
+      const MagneticField& field, const rclcpp::Time& stamp_in) const;
 
 protected:
   //! \brief If true, the magnetic model will fail if it is used outside its bounds.
-  bool strict {true};
+  bool strict_ {true};
 
   //! \brief PIMPL data
-  std::unique_ptr<MagneticModelPrivate> data;
-  RequiredInterfaces node;
+  std::unique_ptr<MagneticModelPrivate> data_;
+  RequiredInterfaces node_;
 };
 
-}
+}  // namespace magnetic_model
